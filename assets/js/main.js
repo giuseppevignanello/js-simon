d45aafb3a99e3ed793ab8df05aea4c72d54c74cc
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-21 14:48:10 +0100

    problem analysis

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+//Get an element of the dom to show the numbers 
+//Take 5 random number with a function and save them in an array
+//Show the prompts with a setTiming function and save them in an array
+// compare the user's array with the random's array 
